484872eb85f546b09e55f8f949b9e4e23915b9ea
krinkin mike <kmu@ilikethinkpad.(none)>
2012-11-13 17:54:59 +0400

    new transaction list adapter

--- src/ru/spbau/WhereIsMyMoney/gui/TransactionsListActivity.java
+++ src/ru/spbau/WhereIsMyMoney/gui/TransactionsListActivity.java
@@ -20,73 +20,92 @@ import java.util.List;
  * Shows list of transactions from specified card.
  */
 public class TransactionsListActivity extends Activity {
-	private static final String FORMAT = "yyyy.MM.dd HH:mm:ss";
-	private static final SimpleDateFormat FORMATTER = new SimpleDateFormat(FORMAT);
+//	private static final String FORMAT = "yyyy.MM.dd HH:mm:ss";
+//	private static final SimpleDateFormat FORMATTER = new SimpleDateFormat(FORMAT);
 	
     static final String ID_PARAM = "id";
     final String OK = "Ok";
     final String TRANSACTION = "Transaction description";
     TransactionLogSource db;
 
-    private void createTransactionsDialog(String title, String message) {
-        AlertDialog.Builder ad = new AlertDialog.Builder(TransactionsListActivity.this);
-        ad.setTitle(title);
-        ad.setMessage(message);
-        ad.setCancelable(true);
-        ad.setPositiveButton(OK, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int arg1) {
-            }
-        });
-        ad.show();
-    }
+//    private void createTransactionsDialog(String title, String message) {
+//        AlertDialog.Builder ad = new AlertDialog.Builder(TransactionsListActivity.this);
+//        ad.setTitle(title);
+//        ad.setMessage(message);
+//        ad.setCancelable(true);
+//        ad.setPositiveButton(OK, new DialogInterface.OnClickListener() {
+//            public void onClick(DialogInterface dialog, int arg1) {
+//            }
+//        });
+//        ad.show();
+//    }
     
-    private String shortDescription(Transaction trans) {
-    	StringBuilder sb = new StringBuilder();
-    	sb.append(FORMATTER.format(trans.getDate()));
-    	if (trans.getDelta() != null) {
-    		sb.append(", delta: ").append(trans.getDelta());
-    	} else {
-    		sb.append(", balance: ").append(trans.getBalance());
-    	}
-    	return sb.toString();
-    }
+//    private String shortDescription(Transaction trans) {
+//    	StringBuilder sb = new StringBuilder();
+//    	sb.append(FORMATTER.format(trans.getDate()));
+//    	if (trans.getDelta() != null) {
+//    		sb.append(", delta: ").append(trans.getDelta());
+//    	} else {
+//    		sb.append(", balance: ").append(trans.getBalance());
+//    	}
+//    	return sb.toString();
+//    }
+//    
+//    private String fullDesription(Transaction trans) {
+//    	StringBuilder sb = new StringBuilder();
+//    	sb.append(FORMATTER.format(trans.getDate())).append("\n");
+//    	sb.append("Balance: ").append(trans.getBalance()).append("\n");
+//    	sb.append("Card: ").append(trans.getCard()).append("\n");
+//    	if (trans.getDelta() != null) {
+//    		sb.append("Delta: ").append(trans.getDelta()).append("\n");
+//    	}
+//    	if (trans.getPlace() != null) {
+//    		sb.append("Place: ").append(trans.getPlace()).append("\n");
+//    	}
+//    	return sb.toString();
+//    }
 
-    private void createListView(final String card) {
-        ListView listView = (ListView) findViewById(ru.spbau.WhereIsMyMoney.R.id.transactions);
+//    private void createListView(final String card) {
+//        ListView listView = (ListView) findViewById(ru.spbau.WhereIsMyMoney.R.id.transactions);
+//
+//        final List<Transaction> transactions = db.getTransactionsPerCard(card);
+//        ArrayAdapter<Transaction> adapter = new TransactionListAdapter(context, transactions);
+        //final String[] values = new String[transactions.size()];
+        //final String[] messages = new String[transactions.size()];
+        //for (int i = 0; i < transactions.size(); ++i) {
+        //	Transaction trans = transactions.get(i);
+        //    values[i] = shortDescription(trans);
+        //    messages[i] = fullDesription(trans);
+        //}
 
-        final List<Transaction> transactions = db.getTransactionsPerCard(card);
-        final String[] values = new String[transactions.size()];
-        final String[] messages = new String[transactions.size()];
-        for (int i = 0; i < transactions.size(); ++i) {
-        	Transaction trans = transactions.get(i);
-            values[i] = shortDescription(trans);
-            messages[i] = trans.toString();
-        }
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        //        android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+//        listView.setAdapter(adapter);
 
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                createTransactionsDialog(TRANSACTION, messages[position]);
-            }
-        });
-    }
+        //listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        //    public void onItemClick(AdapterView<?> parent, View view,
+        //                            int position, long id) {
+        //        createTransactionsDialog(TRANSACTION, messages[position]);
+        //    }
+        //});
+//    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        String message = intent.getStringExtra(ID_PARAM);
+        String card = intent.getStringExtra(ID_PARAM);
         setContentView(ru.spbau.WhereIsMyMoney.R.layout.transactions);
         db = new TransactionLogSource(getApplicationContext());
         db.open();
-        TextView card = (TextView) findViewById(ru.spbau.WhereIsMyMoney.R.id.card_id);
-        card.setText(message);
-        createListView(message);
+        TextView title = (TextView) findViewById(ru.spbau.WhereIsMyMoney.R.id.card_id);
+        title.setText(card);
+//        createListView(message);
+        ListView listView = (ListView) findViewById(ru.spbau.WhereIsMyMoney.R.id.transactions);
+        List<Transaction> transactions = db.getTransactionsPerCard(card);
+        ArrayAdapter<Transaction> adapter = new TransactionListAdapter(getApplicationContext(), transactions);
+        listView.setAdapter(adapter);
     }
 
     @Override
