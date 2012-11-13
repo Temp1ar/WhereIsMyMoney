127b5327c6f6d9ae928c21565d52e185d8323fa0
Alexander Opeykin <alexander.opeykin@gmail.com>
2012-11-13 11:28:03 +0400

    Parser interface

---
+++ src/ru/spbau/WhereIsMyMoney/parser/Parser.java
@@ -0,0 +1,14 @@
+package ru.spbau.WhereIsMyMoney.parser;
+/**
+ * User: Alexander Opeykin alexander.opeykin@gmail.com
+ * Date: 11/12/12
+ * Time: 3:19 PM
+ */
+
+import ru.spbau.WhereIsMyMoney.Transaction;
+
+public interface Parser {
+    public boolean parse(String string, Transaction result);
+    public String getRE();
+    public String getDescription();
+}
