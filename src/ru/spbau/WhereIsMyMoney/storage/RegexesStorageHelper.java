9ea88e954175e77d5556dcad8f827f7492a98f24
krinkin mike <kmu@ilikethinkpad.(none)>
2012-11-13 11:48:31 +0400

    database with serialization deserialization

--- src/ru/spbau/WhereIsMyMoney/storage/RegexesStorageHelper.java
+++ src/ru/spbau/WhereIsMyMoney/storage/RegexesStorageHelper.java
@@ -1,8 +1,5 @@
 package ru.spbau.WhereIsMyMoney.storage;
 
-import java.util.List;
-
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -13,114 +10,36 @@ public class RegexesStorageHelper extends SQLiteOpenHelper {
 	private static final int DATABASE_VERSION = 1;
 	
 	public static final String REGEXES_TABLE = "regexes";
-	public static final String COLUMN_REGEX_ID = "_id";
-	private static final String COLUMN_REGEX_ID_TYPE = "integer primary key autoincrement";
-	
-	public static final String COLUMN_REGEX = "reg";
-	private static final String COLUMN_REGEX_TYPE = "text not null";
-	
-	public static final String COLUMN_CARD = "card";
-	private static final String COLUMN_CARD_TYPE = "integer not null";
-	
-	public static final String COLUMN_BALANCE = "balance";
-	private static final String COLUMN_BALANCE_TYPE = "integer not null";
-	
-	public static final String COLUMN_GROUP = "grp";
-	public static final String COLUMN_GROUP_TYPE = "text not null";
-	
-	public static final String COLUMN_DATE = "date";
-	private static final String COLUMN_DATE_TYPE = "integer";
-	
-	public static final String COLUMN_DESCRIPTION = "description";
-	private static final String COLUMN_DESCRIPTION_TYPE = "text";
-	
-	public static final String COLUMN_DELTA = "delta";
-	private static final String COLUMN_DELTA_TYPE = "text";
-	
-	public static final String COLUMN_PLACE = "place";
-	private static final String COLUMN_PLACE_TYPE = "text";
-	
-	public static final String[] ALL_REGEX_COLUMNS = {
-		COLUMN_REGEX_ID, COLUMN_REGEX, COLUMN_CARD, COLUMN_BALANCE,
-		COLUMN_GROUP, COLUMN_DATE, COLUMN_DESCRIPTION, COLUMN_DELTA,
-		COLUMN_PLACE
-	};
-	
-	private static final String CREATE_REGEXES = "create table " + REGEXES_TABLE + "("
-			+ COLUMN_REGEX_ID    + " " + COLUMN_REGEX_ID_TYPE    + ", "
-			+ COLUMN_REGEX       + " " + COLUMN_REGEX_TYPE       + ", "
-			+ COLUMN_CARD        + " " + COLUMN_CARD_TYPE        + ", "
-			+ COLUMN_BALANCE     + " " + COLUMN_BALANCE_TYPE     + ", "
-			+ COLUMN_GROUP       + " " + COLUMN_GROUP_TYPE       + ", "
-			+ COLUMN_DATE        + " " + COLUMN_DATE_TYPE        + ", "
-			+ COLUMN_DESCRIPTION + " " + COLUMN_DESCRIPTION_TYPE + ", "
-			+ COLUMN_DELTA       + " " + COLUMN_DELTA_TYPE       + ", "
-			+ COLUMN_PLACE       + " " + COLUMN_PLACE_TYPE       + ");";
-	private static final String DROP_REGEXES = "drop table if exists " + REGEXES_TABLE;
-	
-	public static final String GROUPS_TABLE = "groups";
-	public static final String COLUMN_GROUP_ID = "_id";
-	private static final String COLUMN_GROUP_ID_TYPE = "text primary key";
-	
-	public static final String COLUMN_SOURCE = "source";
-	private static final String COLUMN_SOURCE_TYPE = "text not null";
-	
-	public static final String[] ALL_GROUP_COLUMNS = {
-		COLUMN_GROUP_ID, COLUMN_SOURCE
-	};
-	
-	private static final String CREATE_GROUPS = "create table " + GROUPS_TABLE + "("
-			+ COLUMN_GROUP_ID + " " + COLUMN_GROUP_ID_TYPE + ", "
-			+ COLUMN_SOURCE   + " " + COLUMN_SOURCE_TYPE   + ");";
-	private static final String DROP_GROUPS = "drop table if exists " + GROUPS_TABLE;
+	public static final String COLUMN_ID = "_id";
+	private static final String COLUMN_ID_TYPE = "integer primary key autoincrement";
 
-	private RegexStorageParser myParser;
+	public static final String COLUMN_SERIALIZED = "serilized";
+	private static final String COLUMN_SERIALIZED_TYPE = "text not null";
+	
+	public static final String[] ALL_COLUMNS = {
+		COLUMN_ID, COLUMN_SERIALIZED
+	};
+	
+	private static final String CREATE_TABLE = "create table " + REGEXES_TABLE + "("
+			+ COLUMN_ID           + " " + COLUMN_ID_TYPE           + ", "
+			+ COLUMN_SERIALIZED   + " " + COLUMN_SERIALIZED_TYPE   + ");";
+	private static final String DROP_TABLE = "drop table if exists " + REGEXES_TABLE;
 	
 	public RegexesStorageHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-		myParser = new RegexStorageParser(context);
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		Log.d(getClass().getCanonicalName(), "create database " + DATABASE_NAME);
-		db.execSQL(CREATE_GROUPS);
-		db.execSQL(CREATE_REGEXES);
-		List<SmsParserGroup> groups = myParser.getParserGroups();
-		for (SmsParserGroup group : groups) {
-			ContentValues g = new ContentValues();
-			g.put(COLUMN_GROUP_ID, group.getName());
-			g.put(COLUMN_SOURCE, group.getSource());
-			db.insert(GROUPS_TABLE, null, g);
-			for (SmsParserRegex regex: group.getRegexes()) {
-				ContentValues r = new ContentValues();
-				r.put(COLUMN_REGEX, regex.getRegex());
-				r.put(COLUMN_BALANCE, regex.getGroupOf(SmsParserRegex.BALANCE));
-				r.put(COLUMN_CARD, regex.getGroupOf(SmsParserRegex.CARD));
-				r.put(COLUMN_GROUP, group.getName());
-				if (regex.containsGroupOf(SmsParserRegex.DATE)) {
-					r.put(COLUMN_DATE, regex.getGroupOf(SmsParserRegex.DATE));
-				}
-				if (regex.containsGroupOf(SmsParserRegex.DESCRIPTION)) {
-					r.put(COLUMN_DESCRIPTION, regex.getGroupOf(SmsParserRegex.DESCRIPTION));
-				}
-				if (regex.containsGroupOf(SmsParserRegex.DELTA)) {
-					r.put(COLUMN_DELTA, regex.getGroupOf(SmsParserRegex.DELTA));
-				}
-				if (regex.containsGroupOf(SmsParserRegex.PLACE)) {
-					r.put(COLUMN_PLACE, regex.getGroupOf(SmsParserRegex.PLACE));
-				}
-				db.insert(REGEXES_TABLE, null, r);
-			}
-		}
+		db.execSQL(CREATE_TABLE);
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		Log.d(getClass().getCanonicalName(), "update database from "
 				+ oldVersion + " to " + newVersion);
-		db.execSQL(DROP_GROUPS);
-		db.execSQL(DROP_REGEXES);
+		db.execSQL(DROP_TABLE);
 		onCreate(db);
 	}
 
--- src/ru/spbau/WhereIsMyMoney/storage/RegexesStorageSource.java
+++ src/ru/spbau/WhereIsMyMoney/storage/RegexesStorageSource.java
@@ -1,7 +1,15 @@
 package ru.spbau.WhereIsMyMoney.storage;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.StreamCorruptedException;
 import java.util.ArrayList;
 import java.util.List;
+
+import ru.spbau.WhereIsMyMoney.parser.Parser;
 
 import android.content.ContentValues;
 import android.content.Context;
@@ -13,104 +21,42 @@ public class RegexesStorageSource extends BaseDataSource {
 		super(new RegexesStorageHelper(context));
 	}
 	
-	public void addGroup(SmsParserGroup group) {
+	public void addSmsParser(Parser parser) throws IOException {
 		ContentValues g = new ContentValues();
-		g.put(RegexesStorageHelper.COLUMN_GROUP_ID, group.getName());
-		g.put(RegexesStorageHelper.COLUMN_SOURCE, group.getSource());
-		long id = getDatabase().insert(RegexesStorageHelper.GROUPS_TABLE, null, g);
-		Log.d(getClass().getCanonicalName(), "SmsRegexGroup " + group + " saved with id " + id);
+		g.put(RegexesStorageHelper.COLUMN_SERIALIZED, serializeParser(parser));
+		Log.d(getClass().getCanonicalName(), "Parser " + parser + " serialized to database");
 	}
 	
-	private void addRegex(SmsParserGroup group, SmsParserRegex regex) {
-		ContentValues r = new ContentValues();
-		r.put(RegexesStorageHelper.COLUMN_REGEX, regex.getRegex());
-		r.put(RegexesStorageHelper.COLUMN_CARD, regex.getGroupOf(SmsParserRegex.CARD));
-		r.put(RegexesStorageHelper.COLUMN_BALANCE, regex.getGroupOf(SmsParserRegex.BALANCE));
-		r.put(RegexesStorageHelper.COLUMN_GROUP, group.getName());
-		if (regex.getDescription() != null) {
-			r.put(RegexesStorageHelper.COLUMN_DESCRIPTION, regex.getDescription());
-		}
-		if (regex.containsGroupOf(SmsParserRegex.DATE)) {
-			r.put(RegexesStorageHelper.COLUMN_DATE, regex.getGroupOf(SmsParserRegex.DATE));
-		}
-		if (regex.containsGroupOf(SmsParserRegex.DELTA)) {
-			r.put(RegexesStorageHelper.COLUMN_DELTA, regex.getGroupOf(SmsParserRegex.DELTA));
-		}
-		if (regex.containsGroupOf(SmsParserRegex.PLACE)) {
-			r.put(RegexesStorageHelper.COLUMN_PLACE, regex.getGroupOf(SmsParserRegex.PLACE));
-		}
-		long id = getDatabase().insert(RegexesStorageHelper.REGEXES_TABLE, null, r);
-		Log.d(getClass().getCanonicalName(), "SmsRegexRegex " + regex + " saved with id " + id);
-	}
-	
-	public List<SmsParserGroup> getSmsParserGroups(IFilter<SmsParserGroup> filter) {
-		List<SmsParserGroup> groups = new ArrayList<SmsParserGroup>();
-		Cursor cursor = getDatabase().query(RegexesStorageHelper.GROUPS_TABLE,
-				RegexesStorageHelper.ALL_GROUP_COLUMNS,
-				null, null, null, null, null);
-		cursor.moveToFirst();
-		while (!cursor.isAfterLast()) {
-			SmsParserGroup group = cursorToGroup(cursor);
-			if ((filter == null) || filter.match(group)) {
-				groups.add(group);
-			}
-		}
-		cursor.close();
-		return groups;
-	}
-	
-	public List<SmsParserGroup> getAllSmsParserGroups() {
-		return getSmsParserGroups(null);
-	}
-	
-	private SmsParserGroup cursorToGroup(Cursor cursor) {
-		SmsParserGroup group = new SmsParserGroup();
-		group.setName(cursor.getString(cursor.getColumnIndex(RegexesStorageHelper.COLUMN_GROUP_ID)));
-		group.setSource(cursor.getString(cursor.getColumnIndex(RegexesStorageHelper.COLUMN_SOURCE)));
-		return group;
-	}
-	
-	public void getRegexesForGroup(IFilter<SmsParserRegex> filter, SmsParserGroup group) {
+	public List<Parser> getSmsParsers()
+			throws StreamCorruptedException, IOException, ClassNotFoundException {
+		List<Parser> parsers = new ArrayList<Parser>();
 		Cursor cursor = getDatabase().query(RegexesStorageHelper.REGEXES_TABLE,
-				RegexesStorageHelper.ALL_REGEX_COLUMNS,
+				RegexesStorageHelper.ALL_COLUMNS,
 				null, null, null, null, null);
 		cursor.moveToFirst();
 		while (!cursor.isAfterLast()) {
-			SmsParserRegex regex = cursorToRegex(cursor);
-			if ((filter == null) || filter.match(regex)) {
-				group.addRegex(regex);
-			}
+			String serialized = cursor.getString(cursor.getColumnIndex(RegexesStorageHelper.COLUMN_SERIALIZED));
+			parsers.add(deserializeParser(serialized));
 		}
-		cursor.close();		
+		return parsers;
 	}
 	
-	public void getRegexesForGroup(SmsParserGroup group) {
-		getRegexesForGroup(null, group);
+	public static String serializeParser(Parser parser) throws IOException {
+		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+		ObjectOutputStream out = new ObjectOutputStream(bytes);
+		out.writeObject(parser);
+		out.flush();
+		String serialized = bytes.toString();
+		out.close();
+		return serialized;
 	}
 	
-	private SmsParserRegex cursorToRegex(Cursor cursor) {
-		SmsParserRegex regex = new SmsParserRegex();
-		regex.setRegex(cursor.getString(cursor.getColumnIndex(RegexesStorageHelper.COLUMN_REGEX)));
-		regex.setGroupOf(SmsParserRegex.BALANCE, cursor.getInt(
-				cursor.getColumnIndex(RegexesStorageHelper.COLUMN_BALANCE)));
-		regex.setGroupOf(SmsParserRegex.CARD, cursor.getInt(
-				cursor.getColumnIndex(RegexesStorageHelper.COLUMN_CARD)));
-		int column = cursor.getColumnIndex(RegexesStorageHelper.COLUMN_DATE);
-		if (!cursor.isNull(column)) {
-			regex.setGroupOf(SmsParserRegex.DATE, cursor.getInt(column));
-		}
-		column = cursor.getColumnIndex(RegexesStorageHelper.COLUMN_DELTA);
-		if (!cursor.isNull(column)) {
-			regex.setGroupOf(SmsParserRegex.DELTA, cursor.getInt(column));
-		}
-		column = cursor.getColumnIndex(RegexesStorageHelper.COLUMN_DESCRIPTION);
-		if (!cursor.isNull(column)) {
-			regex.setGroupOf(SmsParserRegex.DESCRIPTION, cursor.getInt(column));
-		}
-		column = cursor.getColumnIndex(RegexesStorageHelper.COLUMN_PLACE);
-		if (!cursor.isNull(column)) {
-			regex.setGroupOf(SmsParserRegex.PLACE, cursor.getInt(column));
-		}
-		return regex;
+	public static Parser deserializeParser(String serialized)
+			throws StreamCorruptedException, IOException, ClassNotFoundException {
+		ByteArrayInputStream bytes = new ByteArrayInputStream(serialized.getBytes());
+		ObjectInputStream in = new ObjectInputStream(bytes);
+		Parser parser = (Parser) in.readObject();
+		in.close();
+		return parser;
 	}
 }
